f3ecc822cf44da6899088c30842299f9c4761f10
Charles Anderson <hpcngmoh@gmail.com>
2022-10-17 10:53:05 -0400

    adding template tests

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 export const main = async () => {
   console.log("hello world");
+  return "hello world"
 };
 
 if (require.main === module) {
---
+++ tests/index.test.ts
@@ -0,0 +1,8 @@
+import {describe, expect, test} from '@jest/globals';
+import { main } from "../src";
+
+describe('main entry', () => {
+  test('hello world', async () => {
+    expect(await main()).toBe("hello world");
+  });
+});
