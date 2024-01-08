e4b483126e9c6a5f7c3d56b8bddff103db023e3c
charles <hpcngmoh@gmail.com>
2024-01-08 03:28:25 -0500

    Fixing package.json postinstall setup

--- package.json-modify.js
+++ package.json-modify.js
@@ -1,10 +1,8 @@
 
 const packageJson = require('./package.json');
-const path = require("path");
-const { writeFileSync } = require("fs");
 
-const folderName = path.basename(__dirname)
+const folderName = require("path").basename(__dirname)
 
 packageJson.name = folderName;
 
-writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
+require("fs").writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
