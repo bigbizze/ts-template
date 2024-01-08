cb27faedad56c3e39fe06936c84e4cf08fd15f7b
bigbizze <hpcngmoh@gmail.com>
2024-01-08 03:50:34 -0500

    Making package.json setup better

--- package.json-modify.js
+++ package.json-modify.js
@@ -1,8 +1,25 @@
+// require os module
+
+const getAuthor = () => {
+  let userName;
+  try {
+    userName = require('git-user-name')();
+  } catch {}
+  if (!userName) {
+    userName = require("os").userInfo()?.username;
+  }
+  return userName?.trim() ?? null;
+}
 
 const packageJson = require('./package.json');
 
 const folderName = require("path").basename(__dirname)
 
 packageJson.name = folderName;
+packageJson.author = getAuthor();
+
+delete packageJson.scripts["postinstall"];
+delete packageJson.scripts["init-run"];
+delete packageJson.scripts["modify-package-json"];
 
 require("fs").writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
