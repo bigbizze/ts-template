241f3e4f5c558be7f5e59f1e6d806e1ea9fd8833
bigbizze <hpcngmoh@gmail.com>
2025-05-06 21:56:07 -0400

    Adding autoconfirm for rimraf

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ if [[ ! -f not-yet-installed ]]; then
 fi
 
 node package.json-modify.js
-npx rimraf package.json-modify.js
-yarn remove git-user-name
+npx -y rimraf package.json-modify.js
+pnpm remove git-user-name
 npx rimraf not-yet-installed
 npx rimraf .git
 npx rimraf .idea
